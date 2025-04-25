d84f7736a9311e4d8b97b6c58c86c5266a1f583a
ChestnutZYL <lzyforwebsite@outlook.com>
2025-04-25 09:02:50 +0800

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'workingpapers', 'teachingassitant', 'awards']
+const section_names = ['home', 'publications', 'workingpapers', 'teachingassistant', 'awards']
 
 
 window.addEventListener('DOMContentLoaded', event => {
